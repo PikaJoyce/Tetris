ec4698eeef20254d1b97b62820d6885432f20a83
PikaJoyce <PikaJoyce@users.noreply.github.com>
2019-09-08 23:13:41 -0700

    Updates

---
+++ precache-manifest.9d0e2926f950ec94dad386defc59c127.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e874b9c3a87e47085290921cc3a3dcda",
+    "url": "/Tetris/index.html"
+  },
+  {
+    "revision": "87169233c84e8a050d6d",
+    "url": "/Tetris/static/css/main.4809feb6.chunk.css"
+  },
+  {
+    "revision": "8d0c070868e05a82b3c6",
+    "url": "/Tetris/static/js/2.ff544453.chunk.js"
+  },
+  {
+    "revision": "87169233c84e8a050d6d",
+    "url": "/Tetris/static/js/main.92ff84ee.chunk.js"
+  },
+  {
+    "revision": "f869fd1af1c3a31d7e05",
+    "url": "/Tetris/static/js/runtime~main.aee083e0.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/Tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/Tetris/static/media/bg.2810fe6b.png"
+  }
+]);
